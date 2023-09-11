ad8fc713a29baa4a1f57d20707647b58a6134e22
REVOTRONS-XLABS <REVOTRONS-XLABS@users.noreply.github.com>
2023-09-11 20:35:15 +0530

    CRUD APP COMPLETE

---
+++ CRUD APP/SERVER(BACKEND)/server.js
@@ -0,0 +1,54 @@
+//import express, { json } from "express";
+const express = require("express");
+import cors from "cors";
+
+import { createConnection } from "mysql";
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const db = createConnection(
+    {  
+          host: "localhost",    
+          user: "root",    
+          password: "",    
+          database: "crud"
+    })
+app.get("/", (req, res) => {
+        const sql = "SELECT * FROM student";
+            db.query(sql, (err, data) => {
+                        if(err) return res.json("Error");
+                                return res.json(data);    });});
+
+app.post('/create', (req, res) => {    
+    const sql = "INSERT INTO student (`Name`, `Email`) VALUES (?)";
+        const values = [       
+            req.body.name,        
+            req.body.email    ] 
+           db.query(sql, [values], (err, data) => {    
+                if(err) return res.json("Error");     
+                   return res.json(data);   
+                 })
+    })
+
+app.put('/update/:id', (req, res) => {   
+     const sql = "update student set `Name` = ?, `Email` = ? where ID = ?";    
+     const values = [        req.body.name,        req.body.email    ]    
+     const id = req.params.id;        
+     db.query(sql, [...values, id], (err, data) => {    
+                if(err) return res.json("Error"); 
+                       return res.json(data); 
+                })
+        })
+app.delete('/student/:id', (req, res) => {  
+      const sql = "DELETE FROM student WHERE ID = ?";    
+      const id = req.params.id;   
+          
+      db.query(sql, [id], (err, data) => {  
+                  if(err) return res.json("Error");  
+                return res.json(data); 
+     })
+})
+app.listen(8081, () => {    
+    console.log("listening");})
